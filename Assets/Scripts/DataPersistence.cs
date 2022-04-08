46be7afd157be5192a983d008ce7b275cdc32dd3
Rom1BVT <102188122+Rom1BVT@users.noreply.github.com>
2022-04-09 00:34:33 +0200

    implement data persistence between session
    Add a LoadScore() and SaveScore() method who create a json file with the best score and best player name.

--- Assets/Scripts/DataPersistence.cs
+++ Assets/Scripts/DataPersistence.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class DataPersistence : MonoBehaviour
 {
@@ -14,7 +15,38 @@ public class DataPersistence : MonoBehaviour
         if(Instance != null) { Destroy(gameObject);return;}
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadScore();
     }
 
+    [System.Serializable]
+    class SaveData
+    {
+        public string bestPlayer;
+        public int bestScore;
+    }
 
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.bestPlayer = bestPlayer;
+        data.bestScore = bestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            bestPlayer = data.bestPlayer;
+            bestScore = data.bestScore;
+        }
+
+        
+    }
 }
--- Assets/Scripts/MenuUIManager.cs
+++ Assets/Scripts/MenuUIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -9,10 +10,16 @@ using UnityEditor;
 
 public class MenuUIManager : MonoBehaviour
 {
+    public TextMeshProUGUI bestScoreText;
     // Start is called before the first frame update
     void Start()
     {
         DataPersistence.Instance.playerName = null;
+
+        if (DataPersistence.Instance.bestScore != 0)
+        {
+            bestScoreText.text = $"Best Score\r\n{DataPersistence.Instance.bestPlayer} = {DataPersistence.Instance.bestScore}";
+        }
     }
 
     public void Play()
@@ -25,6 +32,7 @@ public class MenuUIManager : MonoBehaviour
     }
     public void Exit()
     {
+        DataPersistence.Instance.SaveScore();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
